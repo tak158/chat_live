6835473ef3d6fd71411dfa128fdd1cd31b81d52c
snthd <158rune@gmail.com>
2013-06-11 20:09:45 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChatLive::Application.config.secret_key_base = 'cb75a16629621854310b9656efb073d48b1b1f8f74db5bc70874befbc7bc76afd2c5694e2b74478f8a8909d6321186f0cc236c3a92d1252f804f9f5ff94978b9'
